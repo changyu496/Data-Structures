768e012a280110582c2a504239c748b8a36fb9a7
changyu <changyu01@ke.com>
2019-07-28 01:39:12 +0800

    [code]add lookqueue and add some test case,and update readme

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,14 +1,26 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayQueue<Integer> queue = new ArrayQueue<Integer>();
-        for (int i = 0; i < 10; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
-            if (i % 3 == 2) {
-                queue.dequeue();
-                System.out.println(queue);
-            }
+    public static void testQueue(Queue queue,int opCount){
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(random.nextInt());
         }
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+        long start1 = System.nanoTime();
+        testQueue(new ArrayQueue(),opCount);
+        long end1 = System.nanoTime();
+        System.out.println("ArrayQueue :"+(end1-start1)/1000000000.0 +"秒");
+        long start2 = System.nanoTime();
+        testQueue(new LoopQueue(),opCount);
+        long end2 = System.nanoTime();
+        System.out.println("LoopQueue :"+(end2-start2)/1000000000.0 +"秒");
     }
 }
