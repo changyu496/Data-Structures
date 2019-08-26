d39d1eb51480e576c138d900104a6eb6bb354213
changyu <changyu01@ke.com>
2019-08-26 23:57:52 +0800

    [code]add dummyHead

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,20 +1,20 @@
 public class LinkedList<E> {
 
-    private class Node<E>{
+    private class Node<E> {
         public E e;
         public Node next;
 
-        public Node(E e,Node next){
+        public Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
 
-        public Node(E e){
-            this(e,null);
+        public Node(E e) {
+            this(e, null);
         }
 
-        public Node(){
-            this(null,null);
+        public Node() {
+            this(null, null);
         }
 
         @Override
@@ -22,12 +22,13 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
-    private Node<E> head;
+
+    private Node<E> dummyHead;
 
     private int size;
 
-    public LinkedList(){
-        head = null;
+    public LinkedList() {
+        dummyHead = new Node<E>(null, null);
         size = 0;
     }
 
@@ -35,40 +36,32 @@ public class LinkedList<E> {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void addFirst(E e){
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-
-        head = new Node(e,head);
-
-        size ++;
+    public void addFirst(E e) {
+        add(0, e);
     }
 
-    public void add(E e,int index){
-        if(0<index || index>size){
+    public void add(int index, E e) {
+        if (0 < index || index > size) {
             throw new IllegalArgumentException("Add failed,illegal index");
         }
-        if(index == 0){
-            addFirst(e);
-        }else{
-            Node prev = head;
-            for (int i = 0; i < index-1; i++) {
-                prev = prev.next;
-            }
-//            Node newNode = new Node(e);
-//            newNode.next = prev.next;
-//            prev.next = newNode;
-            prev.next = new Node(e,prev.next);
-            size++;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
+//        Node newNode = new Node(e);
+//        newNode.next = prev.next;
+//        prev.next = newNode;
+        prev.next = new Node(e, prev.next);
+        size++;
+
     }
 
-    public void addLast(E e){
-        add(e,size);
+    public void addLast(E e) {
+        add(size, e);
     }
+
 }
